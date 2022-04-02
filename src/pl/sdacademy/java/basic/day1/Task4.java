f87d4a85ad4c4a6236b21d31daa933a62e1b4920
Pawel Dudko <paweldudko@outlook.com>
2022-04-02 12:57:01 +0200

    solution day1_ex4

---
+++ src/pl/sdacademy/java/basic/day1/Task4.java
@@ -0,0 +1,40 @@
+package pl.sdacademy.java.basic.day1;
+
+import java.util.Scanner;
+
+public class Task4 {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final double BMI_MIN_RANGE = 18.5;
+    private static final double BMI_MAX_RANGE = 24.9;
+
+    public static void main(String[] args) {
+        float weight = getWeightFromUser();
+        int height = getHeightFromUser();
+        float bmi = calculateBMI(weight, height);
+        displayBmiResult(bmi);
+    }
+
+    private static float getWeightFromUser() {
+        System.out.print("Please insert weight in kg: ");
+        return scanner.nextFloat();
+    }
+
+    private static int getHeightFromUser() {
+        System.out.print("Please insert height in cm: ");
+        return scanner.nextInt();
+    }
+
+    private static float calculateBMI(float weight, int height) {
+        //float heightInMeter = (float)height/100;
+        float heightInMeter = height/100F;
+        return weight / (float) Math.pow(heightInMeter, 2);
+    }
+
+    private static void displayBmiResult(float bmi) {
+        if(bmi < BMI_MIN_RANGE || bmi > BMI_MAX_RANGE) {
+            System.out.println("BMI is not correct: " + bmi);
+        } else {
+            System.out.println("BMI is correct: " + bmi);
+        }
+    }
+}
